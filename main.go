442d78b9b1b24297060d386381d4eaa7fcfe73d9
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-12-09 00:35:15 +0700

    long list

--- main.go
+++ main.go
@@ -2,149 +2,81 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
-	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/erikgeiser/promptkit/selection"
 )
 
 type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
+	items     []string
+	result    string
+	selection *selection.Model[string]
 }
 
-func initialModel() model {
-	return model{
-		choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
-		selected: make(map[int]struct{}),
+func initialModel() *model {
+	return &model{
+		items: []string{"Carrots", "Celery", "Kohlrabi", "Cabbage", "Turnips", "Radishes", "Beets", "Potatoes", "Onions", "Garlic", "Shallots", "Leeks", "Scallions", "Chives", "Ginger", "Turmeric", "Rutabaga", "Parsnips", "Horseradish", "Wasabi", "Daikon", "Jicama", "Water chestnuts", "Yams", "Cassava", "Taro", "Lotus root", "Burdock", "Salsify", "Skirret", "Malanga", "Turnip root", "Crosne", "Ginger root"},
 	}
 }
 
-func (m model) Init() tea.Cmd {
-	return nil
+func (m *model) Init() tea.Cmd {
+	sel := selection.New("", m.items)
+	sel.Filter = nil
+	sel.PageSize = 7
+
+	m.selection = selection.NewModel(sel)
+	return m.selection.Init()
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
 	case tea.KeyMsg:
 
 		switch msg.String() {
 
-		case "ctrl+c", "q":
+		case "ctrl+c", "q", "esc":
 			return m, tea.Quit
 
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
 		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
+			if m.result != "" {
+				return m, tea.Quit
 			}
+
+			res, err := m.selection.Value()
+			if err != nil {
+				return m, tea.Quit
+			}
+
+			m.result = res
+			return m, nil
+
+		default:
+			_, cmd := m.selection.Update(msg)
+
+			return m, cmd
 		}
 
 	}
 	return m, nil
 }
 
-func (m model) View() string {
+func (m *model) View() string {
 	s := "What would you like to buy today?\n\n"
 
-	for i, choice := range m.choices {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
-		}
+	s += m.selection.View()
 
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
-
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	if m.result != "" {
+		s += fmt.Sprintf("\nYou bought %s\n", m.result)
 	}
 
 	s += "\nPress q to quit.\n"
 	return s
 }
 
-const url = "https://santus.dev/"
-
-type model2 struct {
-	status int
-	err    error
-}
-
-func checkSomeUrl(url string) tea.Cmd {
-	return func() tea.Msg {
-		c := &http.Client{Timeout: 10 * time.Second}
-		res, err := c.Get(url)
-
-		if err != nil {
-			return errMsg{err}
-		}
-
-		return statusMsg(res.StatusCode)
-	}
-}
-
-type statusMsg int
-
-type errMsg struct { err error }
-
-func (e errMsg) Error() string { return e.err.Error() }
-
-func (m model2) Init() (tea.Cmd) {
-	return checkSomeUrl(url)
-}
-
-func (m model2) Update (msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	case statusMsg:
-		m.status = int(msg)
-		return m, tea.Quit
-
-	case errMsg:
-		m.err = msg
-		return m, tea.Quit
-
-	case tea.KeyMsg:
-		if msg.Type == tea.KeyCtrlC {
-			return m, tea.Quit
-		}
-	}
-	return m, nil
-}
-
-func (m model2) View() string {
-	if m.err != nil {
-		return fmt.Sprintf("\nWe had some trouble with the server: %v\n\n", m.err)
-	}
-
-	s := fmt.Sprintf("Checking %s ... ", url)
-
-	if m.status > 0 {
-		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
-	}
-
-	return "\n" + s + "\n\n"
-}
-
 func main() {
-	p := tea.NewProgram(model2{})
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Could not start program:", err)
 		os.Exit(1)
