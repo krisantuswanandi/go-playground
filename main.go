8d3dea55bb485feda5f2b9d0a02debcb02da4c54
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-12-08 23:39:04 +0700

    bubbletea commands

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -78,8 +80,71 @@ func (m model) View() string {
 	return s
 }
 
+const url = "https://santus.dev/"
+
+type model2 struct {
+	status int
+	err    error
+}
+
+func checkSomeUrl(url string) tea.Cmd {
+	return func() tea.Msg {
+		c := &http.Client{Timeout: 10 * time.Second}
+		res, err := c.Get(url)
+
+		if err != nil {
+			return errMsg{err}
+		}
+
+		return statusMsg(res.StatusCode)
+	}
+}
+
+type statusMsg int
+
+type errMsg struct { err error }
+
+func (e errMsg) Error() string { return e.err.Error() }
+
+func (m model2) Init() (tea.Cmd) {
+	return checkSomeUrl(url)
+}
+
+func (m model2) Update (msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case statusMsg:
+		m.status = int(msg)
+		return m, tea.Quit
+
+	case errMsg:
+		m.err = msg
+		return m, tea.Quit
+
+	case tea.KeyMsg:
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model2) View() string {
+	if m.err != nil {
+		return fmt.Sprintf("\nWe had some trouble with the server: %v\n\n", m.err)
+	}
+
+	s := fmt.Sprintf("Checking %s ... ", url)
+
+	if m.status > 0 {
+		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
+	}
+
+	return "\n" + s + "\n\n"
+}
+
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(model2{})
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Could not start program:", err)
 		os.Exit(1)
