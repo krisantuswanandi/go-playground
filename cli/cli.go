9f5e87dc13a380e4c191d6bda8e0bcd464d19abc
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-12-18 12:48:45 +0700

    cli flag

--- cli/cli.go
+++ cli/cli.go
@@ -12,8 +12,36 @@ func RunCli() {
 	app := &cli.App{
 		Name:  "boom",
 		Usage: "make an explosive entrance",
-		Action: func(*cli.Context) error {
-			fmt.Println("boom! I say!")
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "version",
+				Usage:   "show version",
+				Aliases: []string{"v"},
+			},
+			&cli.StringFlag{
+				Name:    "lang",
+				Value:   "english",
+				Usage:   "select `language`",
+				Aliases: []string{"l"},
+				Action: func(ctx *cli.Context, v string) error {
+					if v != "english" && v != "indonesia" {
+						return fmt.Errorf("invalid language. Please use 'english' or 'indonesia'")
+					}
+					return nil
+				},
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			if cCtx.Bool("version") {
+				fmt.Println("Version: 0.0.1")
+				return nil
+			}
+
+			if cCtx.String("lang") == "indonesia" {
+				fmt.Println("Halo Dunia!")
+			} else {
+				fmt.Println("Hello World!")
+			}
 			return nil
 		},
 	}
