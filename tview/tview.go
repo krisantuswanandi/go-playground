d43a1b17645131856f5328513feb957536b1a4af
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-12-20 23:37:58 +0700

    tview flex

--- tview/tview.go
+++ tview/tview.go
@@ -1,12 +1,22 @@
 package tview
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func Run() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	app := tview.NewApplication()
+
+	boxTop := tview.NewBox().SetBorder(true).SetTitle("Top").SetBackgroundColor(tcell.ColorDefault)
+	boxBottom := tview.NewBox().SetBorder(true).SetTitle("Bottom").SetBackgroundColor(tcell.ColorDefault)
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(boxTop, 10, 1, false).
+		AddItem(boxBottom, 0, 1, false)
+
+	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
 }
